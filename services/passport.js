10091a8d932eb3fed6e89488afb7d4de3714e539
naivedeveloper95 <findsatyam95@gmail.com>
2020-05-02 18:20:09 +0530

    Added formatting fix

--- services/passport.js
+++ services/passport.js
@@ -1,42 +1,42 @@
-const passport = require("passport");
-const GoogleStrategy = require("passport-google-oauth20");
-const mongoose = require("mongoose");
-const keys = require("../config/keys");
+const passport = require('passport')
+const GoogleStrategy = require('passport-google-oauth20')
+const mongoose = require('mongoose')
+const keys = require('../config/keys')
 
-const User = mongoose.model("User");
+const User = mongoose.model('User')
 
 passport.serializeUser((user, done) => {
-  done(null, user.id);
-});
+  done(null, user.id)
+})
 
 passport.deserializeUser((id, done) => {
   User.findById(id).then((user) => {
-    done(null, user);
-  });
-});
+    done(null, user)
+  })
+})
 
 passport.use(
   new GoogleStrategy(
     {
-      callbackURL: "/auth/google/callback",
+      callbackURL: '/auth/google/callback',
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
-      proxy: true,
+      proxy: true
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
-        const existingUser = await User.findOne({ googleId: profile.id });
+        const existingUser = await User.findOne({ googleId: profile.id })
         if (existingUser) {
-          return done(null, existingUser);
+          return done(null, existingUser)
         }
         const user = await new User({
           googleId: profile.id,
-          displayName: profile.displayName,
-        }).save();
-        done(null, user);
+          displayName: profile.displayName
+        }).save()
+        done(null, user)
       } catch (err) {
-        done(err, null);
+        done(err, null)
       }
     }
   )
-);
+)
