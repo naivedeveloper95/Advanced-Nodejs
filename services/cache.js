8c1685114a89f2f6e909464c507def390d01e9ad
naivedeveloper95 <findsatyam95@gmail.com>
2020-05-05 16:26:11 +0530

    Completed caching

--- services/cache.js
+++ services/cache.js
@@ -1,15 +1,16 @@
 const mongoose = require('mongoose')
 const redis = require('redis')
 const util = require('util')
+const keys = require('../config/keys')
 
-const redisUrl = 'redis://127.0.0.1:6379'
-const client = redis.createClient(redisUrl)
-
-client.get = util.promisify(client.get)
+const client = redis.createClient(keys.redisUrl)
+client.hget = util.promisify(client.hget)
 const exec = mongoose.Query.prototype.exec
 
-mongoose.Query.prototype.cache = function () {
+mongoose.Query.prototype.cache = function (options = {}) {
   this.useCache = true
+  this.hashKey = JSON.stringify(options.key || '')
+
   return this
 }
 
@@ -17,16 +18,17 @@ mongoose.Query.prototype.exec = async function () {
   if (!this.useCache) {
     return exec.apply(this, arguments)
   }
+
   const key = JSON.stringify(
     Object.assign({}, this.getQuery(), {
-      Collection: this.mongooseCollection.name
+      collection: this.mongooseCollection.name
     })
   )
 
-  //See if we have a value for 'key' in redis
-  const cacheValue = await client.get(key)
+  // See if we have a value for 'key' in redis
+  const cacheValue = await client.hget(this.hashKey, key)
 
-  //If we do
+  // If we do, return that
   if (cacheValue) {
     const doc = JSON.parse(cacheValue)
 
@@ -35,8 +37,16 @@ mongoose.Query.prototype.exec = async function () {
       : new this.model(doc)
   }
 
-  //Otherwise, issue thge query and store the result in redis
+  // Otherwise, issue the query and store the result in redis
   const result = await exec.apply(this, arguments)
-  client.set(key, JSON.stringify(result), 'EX', 10)
+
+  client.hset(this.hashKey, key, JSON.stringify(result), 'EX', 10)
+
   return result
+}
+
+module.exports = {
+  clearHash(hashKey) {
+    client.del(JSON.stringify(hashKey))
+  }
 }
