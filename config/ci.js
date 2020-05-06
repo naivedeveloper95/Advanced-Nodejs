d03609fe349195d2d533abe8fec5a493546cb0d9
naivedeveloper95 <findsatyam95@gmail.com>
2020-05-06 19:27:15 +0530

    Added CI

--- config/ci.js
+++ config/ci.js
@@ -2,7 +2,8 @@ module.exports = {
   googleClientID:
     '1006733750057-md550crta8ta7rhi131i6jdp8t3knai4.apps.googleusercontent.com',
   googleClientSecret: 'thviteYWLrIJ4maFEBo9J4jc',
-  mongoURI: 'mongodb://127.0.0.1:27017/blogs_ci',
-  cookieKey: '123123123',
+  mongoURI:
+    'mongodb+srv://satyam95:newPassword123@cluster0-c4nwc.mongodb.net/test?retryWrites=true&w=majority',
+  cookieKey: '$%*JWCSB12vtty&*^E$',
   redisUrl: 'redis://127.0.0.1:6379'
 }
